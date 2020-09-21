bdcb712e754e4fb9eb769b763343d8385aef1c16
Badrri-Narayanan <badrri1995@gmail.com>
2020-09-21 22:03:51 +0530

    Adding a Node.js Server for the Angular.js Front-End App

---
+++ BackEndServer.js
@@ -0,0 +1,47 @@
+//Back-End for Hotel Badrri Bhavan using node http server
+const http = require('http');
+const pg = require('pg');
+
+let menu = [];
+
+const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+    'Access-Control-Max-Age': 2592000, // 30 days
+    'Content-Type':'application/JSON'
+    /** add other headers as per requirement */
+};
+
+http.createServer(async (req, res) =>{
+    //A new connection to DB must be established every time.
+    const client = new pg.Client({
+        user: 'postgres',
+        host: 'localhost',
+        database: 'hotel',
+        password: 'password123',
+        port: 5432,
+    })
+    client.connect();
+    try {
+        menu = [];
+        const res = await client.query("select * from menu");
+        for(row of res.rows) {
+            let obj =   {
+                            'name': row.name,
+                            'price': row.price,
+            };
+            menu.push(obj);
+        }
+    } catch(e) {
+        console.error(e.stack);
+    }
+    //DB connection must be closed after querying
+    await client.end();
+    //console.log("Database disconnected");
+    res.writeHead(200, headers);
+    res.write(JSON.stringify(menu));
+    res.end();
+}).listen(3500, ()=>{
+    console.log("BackEnd Server online");
+});
+//await client.end();
